cd71d75d335f396fde3031bbd4f8e516d95ca0a8
Spidy123222 <64176728+Spidy123222@users.noreply.github.com>
2022-08-28 13:02:06 -0700

    Redo trustedlink
    Signed-off-by: Spidy123222 <64176728+Spidy123222@users.noreply.github.com>

--- AltStore/Operations/FetchTrustedSourcesOperation.swift
+++ AltStore/Operations/FetchTrustedSourcesOperation.swift
@@ -11,9 +11,9 @@ import Foundation
 private extension URL
 {
     #if STAGING
-    static let trustedSources = URL(string: "https://raw.githubusercontent.com/Spidy123222/SideStore/change-source/apps.json")!
+    static let trustedSources = URL(string: "https://raw.githubusercontent.com/Spidy123222/SideStore/change-source/trustedapps.json")!
     #else
-    static let trustedSources = URL(string: "https://raw.githubusercontent.com/Spidy123222/SideStore/change-source/apps.json")!
+    static let trustedSources = URL(string: "https://raw.githubusercontent.com/Spidy123222/SideStore/change-source/trustedapps.json")!
     #endif
 }
 
