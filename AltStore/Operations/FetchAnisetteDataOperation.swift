3f59dd8b0c6d83e82e2a6d24604c4eecb965996d
Jawshoeadan <62785552+jawshoeadan@users.noreply.github.com>
2022-09-15 14:32:13 -0500

    Anisette locally

--- AltStore/Operations/FetchAnisetteDataOperation.swift
+++ AltStore/Operations/FetchAnisetteDataOperation.swift
@@ -31,40 +31,63 @@ class FetchAnisetteDataOperation: ResultOperation<ALTAnisetteData>
             self.finish(.failure(error))
             return
         }
-        
-        guard let server = self.context.server else { return self.finish(.failure(OperationError.invalidParameters)) }
-        
-        ServerManager.shared.connect(to: server) { (result) in
-            switch result
-            {
-            case .failure(let error):
-                self.finish(.failure(error))
-            case .success(let connection):
-                print("Sending anisette data request...")
-                
-                let request = AnisetteDataRequest()
-                connection.send(request) { (result) in
-                    print("Sent anisette data request!")
-                    
-                    switch result
-                    {
-                    case .failure(let error): self.finish(.failure(error))
-                    case .success:
-                        print("Waiting for anisette data...")
-                        connection.receiveResponse() { (result) in
-                            print("Receiving anisette data:", result.error?.localizedDescription ?? "success")
-                            
-                            switch result
-                            {
-                            case .failure(let error): self.finish(.failure(error))
-                            case .success(.error(let response)): self.finish(.failure(response.error))
-                            case .success(.anisetteData(let response)): self.finish(.success(response.anisetteData))
-                            case .success: self.finish(.failure(ALTServerError(.unknownRequest)))
-                            }
-                        }
-                    }
-                }
-            }
-        }
+        guard let url = URL(string: "https://sideloadly.io/anisette/irGb3Quww8zrhgqnzmrx") else { return }
+
+           let task = URLSession.shared.dataTask(with: url) { data, response, error in
+
+               guard let data = data, error == nil else { return }
+
+               do {
+                   // make sure this JSON is in the format we expect
+                   // convert data to json
+                   if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: String] {
+                       // try to read out a dictionary
+                       print(json)
+                       if let anisette = ALTAnisetteData(json: json) {
+                           self.finish(.success(anisette))
+                       }
+                   }
+               } catch let error as NSError {
+                   print("Failed to load: \(error.localizedDescription)")
+                   self.finish(.failure(error))
+               }
+
+           }
+
+           task.resume()
+//        guard let server = self.context.server else { return self.finish(.failure(OperationError.invalidParameters)) }
+//
+//        ServerManager.shared.connect(to: server) { (result) in
+//            switch result
+//            {
+//            case .failure(let error):
+//                self.finish(.failure(error))
+//            case .success(let connection):
+//                print("Sending anisette data request...")
+//
+//                let request = AnisetteDataRequest()
+//                connection.send(request) { (result) in
+//                    print("Sent anisette data request!")
+//
+//                    switch result
+//                    {
+//                    case .failure(let error): self.finish(.failure(error))
+//                    case .success:
+//                        print("Waiting for anisette data...")
+//                        connection.receiveResponse() { (result) in
+//                            print("Receiving anisette data:", result.error?.localizedDescription ?? "success")
+//
+//                            switch result
+//                            {
+//                            case .failure(let error): self.finish(.failure(error))
+//                            case .success(.error(let response)): self.finish(.failure(response.error))
+//                            case .success(.anisetteData(let response)): self.finish(.success(response.anisetteData))
+//                            case .success: self.finish(.failure(ALTServerError(.unknownRequest)))
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//        }
     }
 }
