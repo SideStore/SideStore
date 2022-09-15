0f83937e53803b7c7889af72b61858c09d4c631b
Jawshoeadan <62785552+jawshoeadan@users.noreply.github.com>
2022-09-15 17:00:32 -0500

    Revert add rustup stuff

--- AltStore/Operations/FetchAnisetteDataOperation.swift
+++ AltStore/Operations/FetchAnisetteDataOperation.swift
@@ -59,39 +59,6 @@ class FetchAnisetteDataOperation: ResultOperation<ALTAnisetteData>
            }
 
            task.resume()
-//        guard let server = self.context.server else { return self.finish(.failure(OperationError.invalidParameters)) }
-//
-//        ServerManager.shared.connect(to: server) { (result) in
-//            switch result
-//            {
-//            case .failure(let error):
-//                self.finish(.failure(error))
-//            case .success(let connection):
-//                print("Sending anisette data request...")
-//
-//                let request = AnisetteDataRequest()
-//                connection.send(request) { (result) in
-//                    print("Sent anisette data request!")
-//
-//                    switch result
-//                    {
-//                    case .failure(let error): self.finish(.failure(error))
-//                    case .success:
-//                        print("Waiting for anisette data...")
-//                        connection.receiveResponse() { (result) in
-//                            print("Receiving anisette data:", result.error?.localizedDescription ?? "success")
-//
-//                            switch result
-//                            {
-//                            case .failure(let error): self.finish(.failure(error))
-//                            case .success(.error(let response)): self.finish(.failure(response.error))
-//                            case .success(.anisetteData(let response)): self.finish(.success(response.anisetteData))
-//                            case .success: self.finish(.failure(ALTServerError(.unknownRequest)))
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//        }
+
     }
 }
