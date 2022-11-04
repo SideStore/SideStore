8c8625908df0e3ae6885122e641fed5dff2a9a13
Riley Testut <riley@rileytestut.com>
2022-11-03 19:08:12 -0500

    Makes OperationError.Code start at 1000
    “Connection errors” subsection starts at 1200.

--- AltStore/Operations/OperationError.swift
+++ AltStore/Operations/OperationError.swift
@@ -18,7 +18,7 @@ extension OperationError
         typealias Error = OperationError
         
         /* General */
-        case unknown
+        case unknown = 1000
         case unknownResult
         case cancelled
         case timedOut
@@ -33,7 +33,7 @@ extension OperationError
         case missingAppGroup
         
         /* Connection */
-        case serverNotFound
+        case serverNotFound = 1200
         case connectionFailed
         case connectionDropped
     }
