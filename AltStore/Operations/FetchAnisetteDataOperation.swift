e3caef48e2ec2703db884b313b06bb801d1d943a
naturecodevoid <44983869+naturecodevoid@users.noreply.github.com>
2023-05-17 07:37:04 -0700

    fixes(anisette v3): improve errors, fix v3 server check, fix some edge cases where SideStore could crash and instead return an error, retry on -45061

--- AltStore/Operations/FetchAnisetteDataOperation.swift
+++ AltStore/Operations/FetchAnisetteDataOperation.swift
@@ -56,14 +56,22 @@ final class FetchAnisetteDataOperation: ResultOperation<ALTAnisetteData>, WebSoc
         }
     }
     
+    // MARK: - COMMON
+    
     func extractAnisetteData(_ data: Data, _ response: HTTPURLResponse?, v3: Bool) throws {
         // make sure this JSON is in the format we expect
         // convert data to json
         if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: String] {
             if v3 {
                 if json["result"] == "GetHeadersError" {
-                    print("Error getting V3 headers: \(json["message"]!)")
-                    throw OperationError.anisetteV3Error(message: json["message"]!)
+                    let message = json["message"]
+                    print("Error getting V3 headers: \(message ?? "no message")")
+                    if let message = message,
+                       message.contains("-45061") {
+                        print("Error message contains -45061 (not provisioned), resetting adi.pb and retrying")
+                        Keychain.shared.adiPb = nil
+                        return provision()
+                    } else { throw OperationError.anisetteV3Error(message: message ?? "Unknown error") }
                 }
             }
             
@@ -112,19 +120,21 @@ final class FetchAnisetteDataOperation: ResultOperation<ALTAnisetteData>, WebSoc
             } else {
                 print("Anisette is invalid!!!!")
                 if v3 {
-                    throw OperationError.anisetteV3Error(message: "Invalid anisette")
+                    throw OperationError.anisetteV3Error(message: "Invalid anisette (the returned data may not have all the required fields)")
                 } else {
-                    throw OperationError.anisetteV1Error
+                    throw OperationError.anisetteV1Error(message: "Invalid anisette (the returned data may not have all the required fields)")
                 }
             }
         } else {
             if v3 {
-                throw OperationError.anisetteV3Error(message: "Invalid anisette format")
+                throw OperationError.anisetteV3Error(message: "Invalid anisette (the returned data may not be in JSON)")
             } else {
-                throw OperationError.anisetteV1Error
+                throw OperationError.anisetteV1Error(message: "Invalid anisette (the returned data may not be in JSON)")
             }
         }
     }
+    
+    // MARK: - V1
     
     func handleV1() {
         print("Server is V1")
@@ -161,7 +171,7 @@ final class FetchAnisetteDataOperation: ResultOperation<ALTAnisetteData>, WebSoc
         print("Fetching anisette V1")
         URLSession.shared.dataTask(with: self.url!) { data, response, error in
             do {
-                guard let data = data, error == nil else { throw OperationError.anisetteV1Error }
+                guard let data = data, error == nil else { throw OperationError.anisetteV1Error(message: "Unable to fetch data\(error != nil ? " (\(error!.localizedDescription))" : "")") }
                 
                 try self.extractAnisetteData(data, response as? HTTPURLResponse, v3: false)
             } catch let error as NSError {
@@ -171,59 +181,7 @@ final class FetchAnisetteDataOperation: ResultOperation<ALTAnisetteData>, WebSoc
         }.resume()
     }
     
-    func fetchClientInfo(_ callback: @escaping () -> Void) {
-        if  self.clientInfo != nil &&
-            self.userAgent != nil &&
-            self.mdLu != nil &&
-            self.deviceId != nil &&
-            Keychain.shared.identifier != nil {
-            print("Skipping client_info fetch since all the properties we need aren't nil")
-            return callback()
-        }
-        print("Trying to get client_info")
-        let clientInfoURL = self.url!.appendingPathComponent("v3").appendingPathComponent("client_info")
-        URLSession.shared.dataTask(with: clientInfoURL) { data, response, error in
-            do {
-                guard let data = data, error == nil else { throw OperationError.anisetteV3Error(message: "Couldn't fetch client info") }
-                
-                if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: String] {
-                    if let clientInfo = json["client_info"] {
-                        print("Server is V3")
-                        
-                        self.clientInfo = clientInfo
-                        self.userAgent = json["user_agent"]!
-                        print("Client-Info: \(self.clientInfo!)")
-                        print("User-Agent: \(self.userAgent!)")
-                        
-                        if Keychain.shared.identifier == nil {
-                            print("Generating identifier")
-                            var bytes = [Int8](repeating: 0, count: 16)
-                            let status = SecRandomCopyBytes(kSecRandomDefault, bytes.count, &bytes)
-                            
-                            if status != errSecSuccess {
-                                print("ERROR GENERATING IDENTIFIER!!! \(status)")
-                                return self.finish(.failure(OperationError.provisioningError(result: "Couldn't generate identifier", message: nil)))
-                            }
-                            
-                            Keychain.shared.identifier = Data(bytes: &bytes, count: bytes.count).base64EncodedString()
-                        }
-                        
-                        let decoded = Data(base64Encoded: Keychain.shared.identifier!)!
-                        self.mdLu = decoded.sha256().hexEncodedString()
-                        print("X-Apple-I-MD-LU: \(self.mdLu!)")
-                        let uuid: UUID = decoded.object()
-                        self.deviceId = uuid.uuidString.uppercased()
-                        print("X-Mme-Device-Id: \(self.deviceId!)")
-                        
-                        callback()
-                    } else { self.handleV1() }
-                } else { self.handleV1() }
-            } catch let error as NSError {
-                print("Failed to load: \(error.localizedDescription)")
-                self.handleV1()
-            }
-        }.resume()
-    }
+    // MARK: - V3: PROVISIONING
     
     func provision() {
         fetchClientInfo {
@@ -231,13 +189,22 @@ final class FetchAnisetteDataOperation: ResultOperation<ALTAnisetteData>, WebSoc
             var request = self.buildAppleRequest(url: URL(string: "https://gsa.apple.com/grandslam/GsService2/lookup")!)
             request.httpMethod = "GET"
             URLSession.shared.dataTask(with: request) { data, response, error in
-                let plist = try! PropertyListSerialization.propertyList(from: data!, format: nil) as! Dictionary<String, Dictionary<String, Any>>
-                self.startProvisioningURL = URL(string: plist["urls"]!["midStartProvisioning"] as! String)!
-                self.endProvisioningURL = URL(string: plist["urls"]!["midFinishProvisioning"] as! String)!
-                print("startProvisioningURL: \(self.startProvisioningURL!.absoluteString)")
-                print("endProvisioningURL: \(self.endProvisioningURL!.absoluteString)")
-                print("Starting a provisioning session")
-                self.startProvisioningSession()
+                if let data = data,
+                   let plist = try? PropertyListSerialization.propertyList(from: data, format: nil) as? Dictionary<String, Dictionary<String, Any>>,
+                   let startProvisioningString = plist["urls"]?["midStartProvisioning"] as? String,
+                   let startProvisioningURL = URL(string: startProvisioningString),
+                   let endProvisioningString = plist["urls"]?["midFinishProvisioning"] as? String,
+                   let endProvisioningURL = URL(string: endProvisioningString) {
+                    self.startProvisioningURL = startProvisioningURL
+                    self.endProvisioningURL = endProvisioningURL
+                    print("startProvisioningURL: \(self.startProvisioningURL!.absoluteString)")
+                    print("endProvisioningURL: \(self.endProvisioningURL!.absoluteString)")
+                    print("Starting a provisioning session")
+                    self.startProvisioningSession()
+                } else {
+                    print("Apple didn't give valid URLs! Got response: \(String(data: data ?? Data("nothing".utf8), encoding: .utf8) ?? "not utf8")")
+                    self.finish(.failure(OperationError.provisioningError(result: "Apple didn't give valid URLs. Please try again later", message: nil)))
+                }
             }.resume()
         }
     }
@@ -256,7 +223,12 @@ final class FetchAnisetteDataOperation: ResultOperation<ALTAnisetteData>, WebSoc
         case .text(let string):
             do {
                 if let json = try JSONSerialization.jsonObject(with: string.data(using: .utf8)!, options: []) as? [String: Any] {
-                    let result = json["result"]! as! String
+                    guard let result = json["result"] as? String else {
+                        print("The server didn't give us a result")
+                        client.disconnect(closeCode: 0)
+                        self.finish(.failure(OperationError.provisioningError(result: "The server didn't give us a result", message: nil)))
+                        return
+                    }
                     print("Received result: \(result)")
                     switch result {
                     case "GiveIdentifier":
@@ -273,15 +245,26 @@ final class FetchAnisetteDataOperation: ResultOperation<ALTAnisetteData>, WebSoc
                         request.httpMethod = "POST"
                         request.httpBody = try! PropertyListSerialization.data(fromPropertyList: body, format: .xml, options: 0)
                         URLSession.shared.dataTask(with: request) { data, response, error in
-                            let plist = try! PropertyListSerialization.propertyList(from: data!, format: nil) as! Dictionary<String, Dictionary<String, Any>>
-                            let spim = plist["Response"]!["spim"] as! String
-                            print("Giving start provisioning data")
-                            client.json(["spim": spim])
+                            if let data = data,
+                               let plist = try? PropertyListSerialization.propertyList(from: data, format: nil) as? Dictionary<String, Dictionary<String, Any>>,
+                               let spim = plist["Response"]?["spim"] as? String {
+                                print("Giving start provisioning data")
+                                client.json(["spim": spim])
+                            } else {
+                                print("Apple didn't give valid start provisioning data! Got response: \(String(data: data ?? Data("nothing".utf8), encoding: .utf8) ?? "not utf8")")
+                                client.disconnect(closeCode: 0)
+                                self.finish(.failure(OperationError.provisioningError(result: "Apple didn't give valid start provisioning data. Please try again later", message: nil)))
+                            }
                         }.resume()
                         
                     case "GiveEndProvisioningData":
                         print("Getting end provisioning data")
-                        let cpim = json["cpim"]! as! String
+                        guard let cpim = json["cpim"] as? String else {
+                            print("The server didn't give us a cpim")
+                            client.disconnect(closeCode: 0)
+                            self.finish(.failure(OperationError.provisioningError(result: "The server didn't give us a cpim", message: nil)))
+                            return
+                        }
                         let body = [
                             "Header": [String: Any](),
                             "Request": [
@@ -292,17 +275,28 @@ final class FetchAnisetteDataOperation: ResultOperation<ALTAnisetteData>, WebSoc
                         request.httpMethod = "POST"
                         request.httpBody = try! PropertyListSerialization.data(fromPropertyList: body, format: .xml, options: 0)
                         URLSession.shared.dataTask(with: request) { data, response, error in
-                            let plist = try! PropertyListSerialization.propertyList(from: data!, format: nil) as! Dictionary<String, Dictionary<String, Any>>
-                            let ptm = plist["Response"]!["ptm"] as! String
-                            let tk = plist["Response"]!["tk"] as! String
-                            print("Giving end provisioning data")
-                            client.json(["ptm": ptm, "tk": tk])
+                            if let data = data,
+                               let plist = try? PropertyListSerialization.propertyList(from: data, format: nil) as? Dictionary<String, Dictionary<String, Any>>,
+                               let ptm = plist["Response"]?["ptm"] as? String,
+                               let tk = plist["Response"]?["tk"] as? String {
+                                print("Giving end provisioning data")
+                                client.json(["ptm": ptm, "tk": tk])
+                            } else {
+                                print("Apple didn't give valid end provisioning data! Got response: \(String(data: data ?? Data("nothing".utf8), encoding: .utf8) ?? "not utf8")")
+                                client.disconnect(closeCode: 0)
+                                self.finish(.failure(OperationError.provisioningError(result: "Apple didn't give valid end provisioning data. Please try again later", message: nil)))
+                            }
                         }.resume()
                         
                     case "ProvisioningSuccess":
                         print("Provisioning succeeded!")
-                        Keychain.shared.adiPb = json["adi_pb"]! as? String
                         client.disconnect(closeCode: 0)
+                        guard let adiPb = json["adi_pb"] as? String else {
+                            print("The server didn't give us an adi.pb file")
+                            self.finish(.failure(OperationError.provisioningError(result: "The server didn't give us an adi.pb file", message: nil)))
+                            return
+                        }
+                        Keychain.shared.adiPb = adiPb
                         self.fetchAnisetteV3(Keychain.shared.identifier!, Keychain.shared.adiPb!)
                         
                     default:
@@ -351,6 +345,64 @@ final class FetchAnisetteDataOperation: ResultOperation<ALTAnisetteData>, WebSoc
         request.setValue(Locale.current.identifier, forHTTPHeaderField: "X-Apple-Locale")
         request.setValue(TimeZone.current.abbreviation(), forHTTPHeaderField: "X-Apple-I-TimeZone")
         return request
+    }
+    
+    // MARK: - V3: FETCHING
+    
+    func fetchClientInfo(_ callback: @escaping () -> Void) {
+        if  self.clientInfo != nil &&
+                self.userAgent != nil &&
+                self.mdLu != nil &&
+                self.deviceId != nil &&
+                Keychain.shared.identifier != nil {
+            print("Skipping client_info fetch since all the properties we need aren't nil")
+            return callback()
+        }
+        print("Trying to get client_info")
+        let clientInfoURL = self.url!.appendingPathComponent("v3").appendingPathComponent("client_info")
+        URLSession.shared.dataTask(with: clientInfoURL) { data, response, error in
+            do {
+                guard let data = data, error == nil else {
+                    return self.finish(.failure(OperationError.anisetteV3Error(message: "Couldn't fetch client info. The server may be down\(error != nil ? " (\(error!.localizedDescription))" : "")")))
+                }
+                
+                if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: String] {
+                    if let clientInfo = json["client_info"] {
+                        print("Server is V3")
+                        
+                        self.clientInfo = clientInfo
+                        self.userAgent = json["user_agent"]!
+                        print("Client-Info: \(self.clientInfo!)")
+                        print("User-Agent: \(self.userAgent!)")
+                        
+                        if Keychain.shared.identifier == nil {
+                            print("Generating identifier")
+                            var bytes = [Int8](repeating: 0, count: 16)
+                            let status = SecRandomCopyBytes(kSecRandomDefault, bytes.count, &bytes)
+                            
+                            if status != errSecSuccess {
+                                print("ERROR GENERATING IDENTIFIER!!! \(status)")
+                                return self.finish(.failure(OperationError.provisioningError(result: "Couldn't generate identifier", message: nil)))
+                            }
+                            
+                            Keychain.shared.identifier = Data(bytes: &bytes, count: bytes.count).base64EncodedString()
+                        }
+                        
+                        let decoded = Data(base64Encoded: Keychain.shared.identifier!)!
+                        self.mdLu = decoded.sha256().hexEncodedString()
+                        print("X-Apple-I-MD-LU: \(self.mdLu!)")
+                        let uuid: UUID = decoded.object()
+                        self.deviceId = uuid.uuidString.uppercased()
+                        print("X-Mme-Device-Id: \(self.deviceId!)")
+                        
+                        callback()
+                    } else { self.handleV1() }
+                } else { self.finish(.failure(OperationError.anisetteV3Error(message: "Couldn't fetch client info. The returned data may not be in JSON"))) }
+            } catch let error as NSError {
+                print("Failed to load: \(error.localizedDescription)")
+                self.handleV1()
+            }
+        }.resume()
     }
     
     func fetchAnisetteV3(_ identifier: String, _ adiPb: String) {
