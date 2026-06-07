db0218d8f33f51042294fd1cd44c8c900d561469
Derad6709 <Derad6709@users.noreply.github.com>
2026-06-08 01:38:09 +0300

    feat: Add "Expiration Time Remaining" App Shortcut

---
+++ AltStore/Intents/App Intents/ExpirationTimeRemainingIntent.swift
@@ -0,0 +1,60 @@
+import AppIntents
+import AltStoreCore
+
+@available(iOS 17.0, *)
+struct ExpirationTimeRemainingIntent: AppIntent
+{
+    static var title: LocalizedStringResource = "Get Time Until Expiration"
+    static var description = IntentDescription("Returns the number of seconds until the SideStore app's signing certificate expires. Returns 0 if the app has already expired.")
+
+    static var parameterSummary: some ParameterSummary {
+        Summary("Get seconds until \(.applicationName) expires")
+    }
+
+    func perform() async throws -> some IntentResult & ReturnsValue<Double>
+    {
+        do
+        {
+            let secondsRemaining = try await self.secondsUntilExpiration()
+            return .result(value: secondsRemaining)
+        }
+        catch
+        {
+            throw IntentError(error)
+        }
+    }
+}
+
+@available(iOS 17.0, *)
+private extension ExpirationTimeRemainingIntent
+{
+    func secondsUntilExpiration() async throws -> Double
+    {
+        if !DatabaseManager.shared.isStarted
+        {
+            try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<Void, Error>) in
+                DatabaseManager.shared.start { error in
+                    if let error
+                    {
+                        continuation.resume(throwing: error)
+                    }
+                    else
+                    {
+                        continuation.resume()
+                    }
+                }
+            }
+        }
+
+        let context = DatabaseManager.shared.persistentContainer.newBackgroundContext()
+        let expirationDate = await context.perform { () -> Date? in
+            guard let sideStoreApp = InstalledApp.fetchAltStore(in: context) else { return nil }
+            return sideStoreApp.expirationDate
+        }
+
+        guard let expirationDate else { throw OperationError.appNotFound(name: nil) }
+
+        let secondsRemaining = max(0, expirationDate.timeIntervalSinceNow)
+        return secondsRemaining
+    }
+}
