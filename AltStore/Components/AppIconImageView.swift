bee0a371dbd411b3c7b38f44a4f367856de3374a
mahee96 <47920326+mahee96@users.noreply.github.com>
2026-06-28 17:00:01 +0530

    use drop shadow for appIcon when an icon has alpha layer (transparency) - appstore apps cannot have alpha layer icon though

--- AltStore/Components/AppIconImageView.swift
+++ AltStore/Components/AppIconImageView.swift
@@ -25,6 +25,22 @@ class AppIconImageView: UIImageView
         }
     }
     
+    override var image: UIImage? {
+        get {
+            return super.image
+        }
+        set {
+            if let newImage = newValue, newImage.hasAlphaChannel
+            {
+                super.image = newImage.withDropShadow(color: .black, radius: 4, offset: CGSize(width: 0, height: 1.5), opacity: 0.25)
+            }
+            else
+            {
+                super.image = newValue
+            }
+        }
+    }
+    
     init(style: Style) 
     {
         self.style = style
@@ -64,6 +80,32 @@ class AppIconImageView: UIImageView
         case .circular:
             let radius = self.bounds.height / 2
             self.layer.cornerRadius = radius
+        }
+    }
+}
+
+private extension UIImage
+{
+    var hasAlphaChannel: Bool {
+        guard let cgImage = self.cgImage else { return false }
+        let alphaInfo = cgImage.alphaInfo
+        return alphaInfo != .none &&
+               alphaInfo != .noneSkipLast &&
+               alphaInfo != .noneSkipFirst
+    }
+    
+    func withDropShadow(color: UIColor, radius: CGFloat, offset: CGSize, opacity: Float) -> UIImage?
+    {
+        let shadowColor = color.withAlphaComponent(CGFloat(opacity)).cgColor
+        
+        let format = UIGraphicsImageRendererFormat()
+        format.scale = self.scale
+        
+        let renderer = UIGraphicsImageRenderer(size: self.size, format: format)
+        return renderer.image { context in
+            let cgContext = context.cgContext
+            cgContext.setShadow(offset: offset, blur: radius, color: shadowColor)
+            self.draw(in: CGRect(origin: .zero, size: self.size))
         }
     }
 }
