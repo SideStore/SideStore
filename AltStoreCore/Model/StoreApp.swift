b5362d59f8592b1eec3ff4154edfa45c2a109500
mahee96 <47920326+mahee96@users.noreply.github.com>
2026-06-30 11:45:47 +0530

    cleanup

--- AltStoreCore/Model/StoreApp.swift
+++ AltStoreCore/Model/StoreApp.swift
@@ -40,13 +40,9 @@ public enum ReleaseTracks: String, CodingKey, CaseIterable
 
 public extension StoreApp
 {
-    #if ALPHA
-    static let altstoreAppID = Bundle.Info.appbundleIdentifier
-    #elseif BETA
-    static let altstoreAppID = Bundle.Info.appbundleIdentifier
-    #else
-    static let altstoreAppID = Bundle.Info.appbundleIdentifier
-    #endif
+    static var altstoreAppID: String {
+        return Bundle.Info.appbundleIdentifier
+    }
     
     static let dolphinAppID = "me.oatmealdome.dolphinios-njb"
 }
